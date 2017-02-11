ace4d1808583e913509abfa73642193d57c53d42
Brad Anderson <brad.anderson87@gmail.com>
2017-02-11 16:00:50 -0600

    Makes a circle.

--- src/assets/scripts/components/Console.js
+++ src/assets/scripts/components/Console.js
@@ -4,10 +4,8 @@ import Inferno from 'inferno';
 export default function Console(props) {
   return (
     <div className="console">
-      <div className="well well--3x">
-        <div className="typ typ--2x typ--alignCenter">
-          {props.message}
-        </div>
+      <div className="typ typ--2x">
+        {props.message}
       </div>
     </div>
   );
