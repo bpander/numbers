991b9c97bede724488b4075580dd6b98a499419b
Brad Anderson <brad.anderson87@gmail.com>
2017-02-16 21:21:39 -0600

    Easy mode.

--- src/assets/scripts/reducers/reducer.js
+++ src/assets/scripts/reducers/reducer.js
@@ -5,12 +5,12 @@ import { randomInt } from 'util/numbers';
 
 const largeSet = [ 25, 50, 75, 100 ];
 const smallSet = [ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10 ];
-const twoBigRestSmall = () => {
+const oneBigRestSmall = () => {
   const _largeSet = [ ...largeSet ];
   const _smallSet = [ ...smallSet ];
   const numbers = flatten([
-    times(2, () => pullAt(_largeSet, randomInt(0, _largeSet.length - 1))),
-    times(4, () => pullAt(_smallSet, randomInt(0, _smallSet.length - 1))),
+    times(1, () => pullAt(_largeSet, randomInt(0, _largeSet.length - 1))),
+    times(5, () => pullAt(_smallSet, randomInt(0, _smallSet.length - 1))),
   ]);
 
   return numbers;
@@ -18,10 +18,10 @@ const twoBigRestSmall = () => {
 
 const initialState = {
   cursor: 0,
-  numbers: twoBigRestSmall(),
+  numbers: oneBigRestSmall(),
   showRulesPrompt: false,
   stream: [],
-  target: randomInt(101, 999),
+  target: randomInt(101, 499),
 };
 
 const reducer = (state = initialState, action) => {
